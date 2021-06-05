2badfd5b519782d4c36eb5ff017a1a02282f37af
kyohe-tkym <kyohe1208@gmail.com>
2021-06-05 04:20:40 +0000

    [Add]comment-Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,8 @@
 class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    unless @book.favorited_by?(current_user)
-      favorite = current_user.favorites.new(book_id: @book.id)
-      favorite.save
-    end
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
   end
 
   def destroy
